b089354ef8c1c555fb9942a4091df3de6eee590d
Emily Highland <emilyhighland@byu.edu>
2021-02-26 18:33:46 -0700

    edit Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,22 +11,29 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         scanner.useDelimiter("\\p{javaWhitespace}+");
 
-        while(scanner.hasNext() && scanner.next() != null) {
+        while(scanner.hasNext()) {
             String command = scanner.next();
             int input1 = scanner.nextInt();
 
-            if ("add".equals(command)) {
-                System.out.println(calculate.add(input1, scanner.nextInt()));
-            } else if ("subtract".equals(command)) {
-                System.out.println(calculate.subtract(input1, scanner.nextInt()));
-            } else if ("multiply".equals(command)) {
-                System.out.println(calculate.multiply(input1, scanner.nextInt()));
-            } else if ("divide".equals(command)) {
-                System.out.println(calculate.divide(input1, scanner.nextInt()));
-            } else if ("fibonacci".equals(command)) {
-                System.out.println(calculate.fibonacciNumberFinder(input1));
-            } else if ("binary".equals(command)) {
-                System.out.println(calculate.intToBinaryNumber(input1));
+            switch (command) {
+                case "add":
+                    System.out.println(calculate.add(input1, scanner.nextInt()));
+                    break;
+                case "subtract":
+                    System.out.println(calculate.subtract(input1, scanner.nextInt()));
+                    break;
+                case "multiply":
+                    System.out.println(calculate.multiply(input1, scanner.nextInt()));
+                    break;
+                case "divide":
+                    System.out.println(calculate.divide(input1, scanner.nextInt()));
+                    break;
+                case "fibonacci":
+                    System.out.println(calculate.fibonacciNumberFinder(input1));
+                    break;
+                case "binary":
+                    System.out.println(calculate.intToBinaryNumber(input1));
+                    break;
             }
         }
     }
