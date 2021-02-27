2a6c719de607f789ce307ecea50d65a9deba1bc1
Emily Highland <emilyhighland@byu.edu>
2021-02-26 18:00:49 -0700

    fixed NullPointerException in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,20 +13,19 @@ public class Main {
         while(scanner.hasNext()) {
             String command = scanner.next();
             int input1 = Integer.getInteger(scanner.next());
-            int input2 = Integer.getInteger(scanner.next());
 
             switch (command) {
                 case "add":
-                    System.out.println(calculate.add(input1, input2));
+                    System.out.println(calculate.add(input1, Integer.getInteger(scanner.next())));
                     break;
                 case "subtract":
-                    System.out.println(calculate.subtract(input1, input2));
+                    System.out.println(calculate.subtract(input1, Integer.getInteger(scanner.next())));
                     break;
                 case "multiply":
-                    System.out.println(calculate.multiply(input1, input2));
+                    System.out.println(calculate.multiply(input1, Integer.getInteger(scanner.next())));
                     break;
                 case "divide":
-                    System.out.println(calculate.divide(input1, input2));
+                    System.out.println(calculate.divide(input1, Integer.getInteger(scanner.next())));
                     break;
                 case "fibonacci":
                     System.out.println(calculate.fibonacciNumberFinder(input1));
